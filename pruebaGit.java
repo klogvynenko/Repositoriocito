0574870ddb254ae1f00c085c4a61fe1c76225d54
Nirvana <trollroot91@gmail.com>
2015-04-21 13:46:45 +0200

    first commit

---
+++ pruebaGit.java
@@ -0,0 +1,12 @@
+public class pruebaGit{
+	public static void main (String[] args){
+	
+	// Autor: String autor = "YO";
+	System.out.println("ola ke ase");
+	
+	String saludo = "Saludo";
+	System.out.println("Otro saludo: "+saludo);
+	String adios = "Adios";
+	// D:
+	}
+}
